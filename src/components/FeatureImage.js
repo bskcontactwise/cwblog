01db051f82408f7b60cbf28f4e6a4e89c017cbc4
bskcontactwise <satheeshkumar@contactwise.io>
2021-04-13 22:04:29 +0530

    FeatureImage partially added

---
+++ src/components/FeatureImage.js
@@ -0,0 +1,25 @@
+import React from "react"
+import {useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import {FeatureImageWrapper} from "../elements"
+
+export const FeatureImage = ({fixed}) => {
+    // const data = useStaticQuery(graphql`
+    // query{
+    //     imageSharp(fixed:{
+
+    //     })
+    // }
+    
+    // `)
+
+    return(
+        
+        <FeatureImageWrapper>
+        {/* <Img /> */}
+        </FeatureImageWrapper>
+            )
+
+}
+
+
