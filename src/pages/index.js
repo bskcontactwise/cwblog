0c11235ece98380dc8454dc2c30b7e4c0f507da0
bskcontactwise <satheeshkumar@contactwise.io>
2021-04-15 12:43:58 +0530

    ContentCard Added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,21 @@
 import React from "react"
-import {Container, FeatureImage} from "../components"
+import {Container, FeatureImage, Content, ContentCard} from "../components"
 
 export default function Home() {
   return (
     <Container>
 
 <FeatureImage/>
+<Content>
+  <ContentCard date="March 22 2021" title="lorem ipsum title fro testing" 
+  excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur metus odio, egestas nec auctor sit amet,
+   porta quis neque. Suspendisse quis velit nunc. Aenean nec lacus aliquet, consequat mauris a, tempus elit. Maecenas
+    iaculis eget tortor ut ornare. Nulla dapibus consectetur tempus. Duis lobortis tristique est, quis lobortis ex dapibus at.
+     Donec ut felis quis erat iaculis consectetur ornare rutrum ligula. Praesent ut tempor felis, at ornare erat. 
+  Nam pulvinar semper tellus. Mauris orci ex, lacinia a nulla vitae, mattis imperdiet est. Nam in leo pharetra, fringilla eros quis, bibendum ipsum. "
+  
+  slug="testing the link" />
+</Content>
    
     </Container>
   )
